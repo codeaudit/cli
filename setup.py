2ad518fe819f56806118dc50ecf4859224c239ce
Henning Peters <pete@dexterslab.de>
2016-11-16 06:10:04 +0100

    bump version

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ import sys
 from setuptools import setup, find_packages
 
 NAME = "riseml"
-VERSION = "0.1.0"
+VERSION = "0.1.1"
 
 
 REQUIRES = ["urllib3 >= 1.15", "six >= 1.10", "certifi", "python-dateutil"]
@@ -23,10 +23,10 @@ REQUIRES += ['pyyaml', 'requests']
 setup(
     name=NAME,
     version=VERSION,
-    description="RiseML API",
+    description="RiseML API client",
     author_email="support@riseml.com",
     url="",
-    keywords=["Swagger", "RiseML API"],
+    keywords=["RiseML"],
     install_requires=REQUIRES,
     packages=find_packages(),
     include_package_data=True,
