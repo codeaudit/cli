fda2f7936e163b346cd7a05dbf3c16afa0441280
Henning Peters <pete@dexterslab.de>
2016-12-20 15:47:49 +0100

    update api and bump version

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ import sys
 from setuptools import setup, find_packages
 
 NAME = "riseml"
-VERSION = "0.2.6"
+VERSION = "0.2.7"
 
 
 REQUIRES = ["urllib3 >= 1.15", "six >= 1.10", "certifi", "python-dateutil"]
