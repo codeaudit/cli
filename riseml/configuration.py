71322e1ebd4ee661cd227ec933c036caa6b61d3b
Henning Peters <pete@dexterslab.de>
2017-01-11 10:13:00 +0100

    bump version

--- riseml/configuration.py
+++ riseml/configuration.py
@@ -221,5 +221,5 @@ class Configuration(object):
                "OS: {env}\n"\
                "Python Version: {pyversion}\n"\
                "Version of the API: 1.1.0\n"\
-               "SDK Package Version: 0.2.8".\
+               "SDK Package Version: 0.2.9".\
                format(env=sys.platform, pyversion=sys.version)
