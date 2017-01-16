84d97e704e9fd5a9d62263eb32132dfd28f4d57c
Henning Peters <pete@dexterslab.de>
2017-01-16 18:37:03 +0100

    support for new riseml spec

--- riseml/config_parser.py
+++ riseml/config_parser.py
@@ -27,38 +27,52 @@ class Config(object):
     description = None
     github_repo = None
     image = None
-    build_commands = None
+    build_commands = []
     build_include = None
-    batch_commands = None
-    service_commands = None
+    batch_commands = []
+    service_commands = []
     service_input = None
     service_output = None
     service_demo = None
 
 
 def parse_list(l):
+    if l is None:
+        return
     if isinstance(l, list):
         return l
-    return l
+    return [l]
+
+
+def parse_one(record):
+    return parse_list(record)[0]
 
 
 def parse_text(text):
     config = Config()
     tmp = yaml.load(text)
-    if not 'image' in tmp:
-        raise ConfigException(u'missing key: image')
 
-    config.title = tmp['title']
-    config.description = tmp['description']
-    config.github_repo = tmp['github_repo']
-    config.image = tmp['image']
-    config.build_commands = parse_list(tmp['build']['commands'])
-    config.build_include = parse_list(tmp['build']['options']['include'])
-    config.batch_commands = parse_list(tmp['batch']['commands'])
-    config.service_commands = parse_list(tmp['service']['commands'])
-    config.service_input = tmp['service']['input']
-    config.service_output = tmp['service']['output']
-    config.service_demo = tmp['service']['options']['demo']
+    config.title = tmp.get('title')
+    config.description = tmp.get('description')
+    config.github_repo = tmp.get('github_repo')
+
+    config.image = parse_one(tmp['image'])
+
+    if tmp.get('build'):
+        config.build_commands = parse_list(tmp['build']['commands']) or []
+        if tmp['build'].get('options'):
+            config.build_include = parse_list(tmp['build']['options'].get('include'))
+
+    if tmp.get('batch'):
+        config.batch_commands = parse_list(tmp['batch']['commands']) or []
+
+    if tmp.get('service'):
+        config.service_commands = parse_list(tmp['service']['commands']) or []
+        config.service_input = tmp['service'].get('input')
+        config.service_output = tmp['service'].get('output')
+        if tmp['service'].get('options'):
+            config.service_demo = tmp['service']['options'].get('demo')
+
     return config
 
 
