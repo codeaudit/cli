02c3583dee3ef3c019933bcb30e5b6e62e42470c
Henning Peters <pete@dexterslab.de>
2017-01-17 13:33:39 +0100

    bump version

--- riseml/api_client.py
+++ riseml/api_client.py
@@ -73,7 +73,7 @@ class ApiClient(object):
             self.host = host
         self.cookie = cookie
         # Set default User-Agent.
-        self.user_agent = 'RiseML/0.2.10/python'
+        self.user_agent = 'RiseML/0.2.11/python'
 
     @property
     def user_agent(self):
