21b9affe4c2f6a6c46169bed86f327bbc3e70e2a
Vitaly <vitaly-nevgen@users.noreply.github.com>
2017-07-27 22:32:25 +0300

    ctrl-c + log-printer abstraction fix

--- riseml/stream.py
+++ riseml/stream.py
@@ -11,50 +11,66 @@ from . import util
 ANSI_ESCAPE_REGEX = re.compile(r'\x1b[^m]*m')
 
 
-def stream_log(url, ids_to_name):
-    def print_log_message(msg):
+class LogPrinter(object):
+    def __init__(self, ids_to_name):
+        self.ids_to_name = ids_to_name
+
+        self.job_ids_color = {
+            id: util.COLOR_CODES.keys()[(i + 1) % len(util.COLOR_CODES)]
+            for i, id in enumerate(self.ids_to_name.keys())
+        }
+
+        self.job_ids_last_color_used = {}
+        self.indentation = max([len(name) for _, name in self.ids_to_name.items()]) + 1
+
+    def _message_prefix(self, msg):
+        job_name = self.ids_to_name[msg['job_id']]
+        color = self.job_ids_color[msg['job_id']]
+        prefix = "{}| ".format(job_name.ljust(self.indentation))
+
+        return util.color_string(color, prefix)
+
+    def print_log_message(self, msg):
         for line in msg['log_lines']:
-            last_color = job_ids_last_color_used.get(msg['job_id'], '')
+            last_color = self.job_ids_last_color_used.get(msg['job_id'], '')
 
             line_text = "[%s] %s" % (util.str_timestamp(line['time']), line['log'])
 
-            output = "%s%s%s%s" % (message_prefix(msg), last_color, line_text, util.ansi_sequence(0))
+            output = "%s%s%s%s" % (self._message_prefix(msg), last_color, line_text, util.ansi_sequence(0))
             used_colors = ANSI_ESCAPE_REGEX.findall(line_text)
-            if used_colors:
-                job_ids_last_color_used[msg['job_id']] = used_colors[-1]
-            print output
 
-    def print_state_message(msg):
+            if used_colors:
+                self.job_ids_last_color_used[msg['job_id']] = used_colors[-1]
+
+            print(output)
+
+    def print_state_message(self, msg):
         state = "[%s] --> %s" % (util.str_timestamp(msg['time']), msg['new_state'])
-        output = "%s%s" % (message_prefix(msg),
+        output = "%s%s" % (self._message_prefix(msg),
                            util.color_string("bold_white", state))
         print(output)
 
-    def message_prefix(msg):
-        job_name = ids_to_name[msg['job_id']]
-        color = job_ids_color[msg['job_id']]
-        prefix = "{}| ".format(job_name.ljust(indentation))
-        return util.color_string(color, prefix)
 
-    job_ids_color = {id: util.COLOR_CODES.keys()[(i + 1) % len(util.COLOR_CODES)]
-                     for i, id in enumerate(ids_to_name.keys())}
-    job_ids_last_color_used = {}
-    indentation = max([len(name) for _,name in ids_to_name.items()]) + 1
+def stream_log(url, ids_to_name):
+    log_printer = LogPrinter(ids_to_name)
 
     def on_message(ws, message):
         msg = json.loads(message)
-
         msg_type = msg['type']
-        if msg_type == 'log':
-            print_log_message(msg)
-        elif msg_type == 'state':
-            print_state_message(msg)
 
-    def on_error(ws, error):
-        handle_error(error)
+        if msg_type == 'log':
+            log_printer.print_log_message(msg)
+        elif msg_type == 'state':
+            log_printer.print_state_message(msg)
+
+    def on_error(ws, e):
+        if isinstance(e, (KeyboardInterrupt, SystemExit)):
+            print('Stopping log..')
+        else:
+            # all other Exception based stuff goes to `handle_error`
+            handle_error(e)
 
     def on_close(ws):
-        # print("Stream closed")
         sys.exit(0)
 
     ws = websocket.WebSocketApp(
