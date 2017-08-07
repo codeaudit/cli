883f0e107302718b5a3b4d7a707ae5039b88cd3d
Vitaly <vitaly-nevgen@users.noreply.github.com>
2017-08-07 14:25:30 +0200

    unhandled http error catcher

--- riseml/user.py
+++ riseml/user.py
@@ -1,9 +1,17 @@
 from riseml.client import DefaultApi, ApiClient
+from riseml.client.rest import ApiException
+
+from riseml.errors import handle_http_error
 from riseml.consts import API_URL
 
 
 def get_user():
     api_client = ApiClient(host=API_URL)
     client = DefaultApi(api_client)
-    user = client.get_user()[0]
+
+    try:
+        user = client.get_user()[0]
+    except ApiException as e:
+        handle_http_error(e.body, e.status)
+
     return user
