5f7dfbc0f56d242bfcbaeda761fc0a44402d13d1
elmarhaussmann <elmar@riseml.com>
2017-08-29 21:06:25 +0800

    Enable node selectors + force build steps in system test

--- riseml/commands/system_test.py
+++ riseml/commands/system_test.py
@@ -1,6 +1,8 @@
 import os
 import tempfile
 import json
+import math
+import random
 
 from riseml.configs import load_config
 from riseml.client import AdminApi, ApiClient
@@ -13,53 +15,76 @@ from riseml.project import push_project
 
 PROJECT_NAME = 'smoke-test'
 
-CONFIG = 'project: %s' % PROJECT_NAME
-CONFIG += """
+JOB_CONFIG = """project: {project_name}
 train:
     image:
         name: ubuntu:16.04
         install:
-        - apt-get -y update
+        - apt-get -y update {more_steps}
         - apt-get -y install stress fortune
     resources:
-        cpus: 1
-        mem: 1024
+        cpus: {num_cpus:.2f}
+        mem: {memory}
         gpus: 0
     run: 
     - bash stress.sh
 """
 
 SCRIPT = """#!/bin/bash
-
-for i in {1..100}
- do
-  stress -c 1 -t 5
- for i in {1..100}
-  do
-    /usr/games/fortune
- done
- 
- sleep 1
+exit
+for i in $(seq 100)
+do
+stress --cpu {threads_cpu} --vm {threads_mem} --vm-bytes {mem_per_thread}M -t 10
+for i in $(seq 100)
+    do
+        /usr/games/fortune
+    done
+sleep 1
 done
 """
 
 
-def prepare_project_dir():
+def get_job_config(num_cpus, memory, force_build_steps):
+    more_steps = ''
+    if force_build_steps:
+        rand = random.randint(0, 10000)
+        more_steps = ('&& echo %s && '
+                       'apt-get -y install python-pip python-dev && '
+                       'pip install --no-binary numpy scipy') % rand
+    return JOB_CONFIG.format(num_cpus=num_cpus, memory=memory, 
+                             project_name=PROJECT_NAME, more_steps=more_steps)
+
+
+def get_script(num_cpus, memory):
+    num_treads = math.ceil(num_cpus)
+    mem_per_thread = int(math.floor(float(memory) / num_treads))
+    return SCRIPT.format(threads_cpu=num_treads, threads_mem=num_treads,
+                         mem_per_thread=mem_per_thread)
+
+
+
+def prepare_project_dir(job_config, stress_script):
     dir = tempfile.mkdtemp()
     config_path = os.path.join(dir, 'riseml.yml')
     with open(config_path, 'w') as f:
-        f.write(CONFIG)
+        f.write(job_config)
     with open(os.path.join(dir, 'stress.sh'), 'w') as f:
-        f.write(SCRIPT)
+        f.write(stress_script)
     return config_path
 
 
 def add_system_test_parser(subparsers):
     parser = subparsers.add_parser('test', help="perform cluster tests")
-    parser.add_argument('--nodename', help="the node name to schedule PODs on", 
+    parser.add_argument('--nodename', help="the node's hostname to schedule jobs on", 
                         required=False)
     parser.add_argument('--num_jobs', help="the number of jobs to run", default=1, 
-                        type=int, required=False)   
+                        type=int, required=False)
+    parser.add_argument('--num_cpus', help="cpus per job to stress", default=1, 
+                        type=float, required=False)
+    parser.add_argument('--mem', help="memory per job to stress", default=1024, 
+                        type=int, required=False)  
+    parser.add_argument('--force_build_steps', help="cause each job to perform considerable build steps",
+                         action="store_const", const=True)
     parser.set_defaults(run=run)
 
 
@@ -67,19 +92,28 @@ def run(args):
     api_client = ApiClient(host=API_URL)
     client = AdminApi(api_client)
     user = get_user()
+    
     for i in range(args.num_jobs):
-        print('Starting job %s of %s' % (i, args.num_jobs))
-        start_job(user, args.nodename)
+        job_config = get_job_config(args.num_cpus, args.mem, args.force_build_steps)
+        stress_script = get_script(args.num_cpus, args.mem)    
+        if i == 0:
+            print('Job configuration:\n\n%s' % job_config)
+        print('Starting job %s of %s to stress %s CPUs and %s MB of memory.' %
+              (i + 1, args.num_jobs, args.num_cpus, args.mem))
+        start_job(user, args.nodename, job_config, stress_script)
 
 
-def start_job(user, nodename):
-    config_path = prepare_project_dir()
+def start_job(user, nodename, job_config, stress_script):
+    config_path = prepare_project_dir(job_config, stress_script)
     config = load_config(config_path)
     revision = push_project(user, PROJECT_NAME, config_path)
     api_client = ApiClient(host=API_URL)
     client = DefaultApi(api_client)
-
+    node_selector = ''
+    if nodename:
+        node_selector = 'kubernetes.io/hostname=%s' % nodename
     experiment = call_api(lambda: client.create_experiment(
         PROJECT_NAME, revision,
-        kind='train', config=json.dumps(config.train.as_dict())
+        kind='train', config=json.dumps(config.train.as_dict()),
+        node_selectors=node_selector
     ))
