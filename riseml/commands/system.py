4c4de95f96a056e2476d78b4b7434a063f8732c5
elmarhaussmann <elmar@riseml.com>
2017-10-30 18:04:52 +0800

    Implement system register

--- riseml/commands/system.py
+++ riseml/commands/system.py
@@ -6,6 +6,7 @@ from .system_test import add_system_test_parser
 def add_system_parser(subparsers):
     parser = subparsers.add_parser('system', help="system level commands")
     subsubparsers = parser.add_subparsers()
+    add_system_register_parser(subsubparsers)    
     add_system_info_parser(subsubparsers)
     add_system_test_parser(subsubparsers)
     def run(args):
@@ -13,3 +14,15 @@ def add_system_parser(subparsers):
     parser.set_defaults(run=run)
 
 
+def add_system_register_parser(subparsers):
+    parser = subparsers.add_parser('register', help="register cluster with account")
+    parser.set_defaults(run=run_register)
+
+
+def run_register(args):
+    print('Please enter the account key you would like to set: ')
+    account_key = input('--> ').strip()
+    api_client = ApiClient()
+    client = AdminApi(api_client)
+    call_api(lambda: client.update_or_create_cluster_info({'account_key': account_key}))        
+    print("Account key set to: %s" % account_key)
